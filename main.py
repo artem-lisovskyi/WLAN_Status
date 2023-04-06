70293fa8425f7e21e711ccf537c8e0ff6e608a69
Artem <artemlis2703@gmail.com>
2023-04-06 11:37:05 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import subprocess
+import time
+
+# Определение имени файла, в который будут записываться значения поля "Signal"
+filename = "signal_values.txt"
+
+while True:
+    # Выполнение команды "netsh wlan show interfaces" в консоли и получение результата
+    result = subprocess.run("netsh wlan show interfaces", capture_output=True, text=True)
+
+    # Поиск значения поля "Signal" в результатах команды
+    signal_value = None
+    for line in result.stdout.split("\n"):
+        if "Signal" in line:
+            signal_value = line.split(":")[-1].strip()
+            break
+
+    # Запись значения поля "Signal" в файл
+    if signal_value:
+        with open(filename, "a") as f:
+            f.write(f"{signal_value}\n")
+
+    # Ожидание 5 минут
+    time.sleep(300)
+
